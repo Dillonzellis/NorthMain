d310834d75f8c6c17d688959cca8570999319a0b
Dillon Ellis <dellis@fcul.com>
2022-07-20 11:43:18 -0400

    format top bar file

--- inc/top-bar.php
+++ inc/top-bar.php
@@ -1,27 +1,26 @@
 <div class="mid-header">
-				<div class="container">
-					<div class="row">
-						<div class="col-md-4">
-							<div class="site-brand">
-								<a class="logo" href="https://www.northmaincu.org/index.php" title="North Main CU">
-									<img src="https://www.northmaincu.org/images/assets/logo2.png" alt="North Main CU">
-								</a>
-							</div><!-- .site-brand -->
-						</div>
+	<div class="container">
+		<div class="row">
+			<div class="col-md-4">
+				<div class="site-brand">
+					<a class="logo" href="https://www.northmaincu.org/index.php" title="North Main CU">
+						<img src="https://www.northmaincu.org/images/assets/logo2.png" alt="North Main CU">
+					</a>
+				</div><!-- .site-brand -->
+			</div>
 
-						
-						<div class="col-md-8" style=" text-align: right;">
-							<div class="header-info-test">
-								<h4 class="hb-header">ONLINE BANKING</h4>
-								<iframe id="tethered_frame" width="426" height="99" src="https://northmaincu-dn.financial-net.com/idp/BD7FB063/tethered" 
-style="position: left; z-index: 100; background: transparent; border: none;" scrolling="no" title="Online Banking Login"></iframe>
-								<p style="margin-bottom: 0 !important; vertical-align: text-bottom; text-transform: uppercase;"><a href="#" data-toggle="modal" data-target="#ezcard" class="button primary rounded" style="background-color: #C30E2E; border: solid 1px;
+
+			<div class="col-md-8" style=" text-align: right;">
+				<div class="header-info-test">
+					<h4 class="hb-header">ONLINE BANKING</h4>
+					<iframe id="tethered_frame" width="426" height="99" src="https://northmaincu-dn.financial-net.com/idp/BD7FB063/tethered" style="position: left; z-index: 100; background: transparent; border: none;" scrolling="no" title="Online Banking Login"></iframe>
+					<p style="margin-bottom: 0 !important; vertical-align: text-bottom; text-transform: uppercase;"><a href="#" data-toggle="modal" data-target="#ezcard" class="button primary rounded" style="background-color: #C30E2E; border: solid 1px;
     border-color: #666666; color: #FFFFFF; font-family: 'Open Sans', Arial, sans-serif; font-size: 12px; font-weight: 700; text-decoration: none;">Credit Card Access</a></p>
-							<!--<iframe frameborder="0" height="50" width="195" marginheight="0" marginwidth="0" scrolling="no" allowtransparency="true" src="https://www.financial-net.com/ethiconcu/Logon?LoginMethod=Express" title="Online Banking"></iframe>-->
-								
-							</div><!-- .header-info -->
-						</div>
+					<!--<iframe frameborder="0" height="50" width="195" marginheight="0" marginwidth="0" scrolling="no" allowtransparency="true" src="https://www.financial-net.com/ethiconcu/Logon?LoginMethod=Express" title="Online Banking"></iframe>-->
 
-					</div><!-- .row -->
-				</div><!-- .container -->
-			</div><!-- .mid-header -->
+				</div><!-- .header-info -->
+			</div>
+
+		</div><!-- .row -->
+	</div><!-- .container -->
+</div><!-- .mid-header -->
