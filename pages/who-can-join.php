cfd87133043a33aeb6bce45f9a51738615b10381
Dillon Ellis <dellis@fcul.com>
2022-07-20 11:44:24 -0400

    original who can joing page

---
+++ pages/who-can-join.php
@@ -0,0 +1,69 @@
+<!doctype html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>North Main Credit Union - Who Can Join</title>
+
+	<?php include '../inc/header.php';?>
+	
+	<script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-85131597-1', 'auto');
+  ga('send', 'pageview');
+
+</script>
+	
+</head>
+
+<body class="home3 header3">
+
+	<div id="wrapper">
+		<header id="header" class="site-header">
+
+			<?php include '../inc/top-bar.php';?>
+			
+			<?php include '../inc/nav.php';?>
+			
+		</header><!-- .site-header -->
+
+		<div id="content" class="site-content">
+			<div class="page-title parallax-window" style="background-image: url('./../images/about-title.jpg'); background-position: top center">
+				<div class="container">
+					<h1>Who Can Join?</h1>
+				</div><!-- .container -->
+			</div><!-- .page-title -->
+
+			<main id="main" class="site-main">
+				<section id="our-university">
+					<div class="container">
+
+						<div class="row">
+							<div class="col-md-12">
+<p>Membership is open to the following:</p>
+<ul>
+    <li>Anyone who works   <strong><em>or</em></strong> lives in Habersham or Banks Counties.&nbsp; </li>
+    <li>Current <strong><em>and</em></strong> retired Ethicon campus      employees, including all contractors and Kelly Associates.</li>
+    <li>Those related by blood, adoption, marriage or living in      the same household as any of the above.</li></ul>
+								<p>&nbsp;</p>
+							</div>
+						</div><!-- .intro-wrap -->
+
+					</div><!-- .container -->
+				</section><!-- #our-university -->
+
+
+			</main><!-- .site-main -->
+		</div><!-- .site-content -->
+
+		<?php include '../inc/footer.php';?>
+						
+	</div><!-- #wrapper -->
+
+	<?php include '../inc/scripts.php';?>
+</body>
+</html>
