87d7428d7c0fe02086a33621b15741e9743b9f7a
Dillon Ellis <dellis@fcul.com>
2022-09-14 14:58:30 -0400

    add original index file

---
+++ index.php
@@ -0,0 +1,217 @@
+<!doctype html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>North Main Credit Union - Home</title>
+
+	<?php include 'inc/header.php';?>
+	
+	<script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-85131597-1', 'auto');
+  ga('send', 'pageview');
+
+</script>
+	<style>
+		.banner {
+		background: black;
+		color: white;
+		padding: 15px 5px;
+		}
+		.banner p {
+			max-width: 900px;
+			margin: 0 auto;
+			text-align: center;
+			font-weight: bold;
+			font-size: 22px;
+			line-height: normal;
+			margin-bottom: 10px;
+		}
+		
+
+			
+
+	</style>
+	
+</head>
+
+<body class="home3 header3">
+   
+	<div id="wrapper">
+		<!--<div class="banner"><p>Members may be able to access Online Banking &amp; the App as it is operating intermittently.</p><p>Our provider is working to resolve connectivity issues, but is still experiencing some disruptions due to power outages.</p><p>We apologize for any inconvenience.</p></div>-->
+		<header id="header" class="site-header">
+
+			<?php include 'inc/top-bar.php';?>
+			
+			<?php include 'inc/nav.php';?>
+			
+		</header><!-- .site-header -->
+		<div id="content" class="site-content">
+			<main id="main" class="site-main">
+				<section class="main-slider fullscreen">
+					<div class="slider">					
+						
+						<!-- .item -->
+							<div class="item">
+							<img src="images/banners/2022/checking.png" alt="Checking">
+							<div class="container">
+								<div class="slider-content">
+								<div class="button-action">
+										<a href="pages/checking-accounts.php" class="button primary rounded" target="_blank" aria-label="Learn More About Checking" style="    margin-top: -60px;">Learn More</a>
+									</div>
+								</div>
+							</div><!-- .container -->
+						</div><!-- .item -->
+						
+						
+						
+						<!-- .item -->
+							<div class="item">
+							<img src="images/banners/2022/summer-vacation.png" alt="Vacation Loan">
+							<div class="container">
+								<div class="slider-content">
+								<div class="button-action">
+										<a href="pages/loans-index.php" class="button primary rounded" target="_blank" aria-label="Learn More About Summer Vacation" style="    margin-top: -60px;">Learn More</a>
+									</div>
+								</div>
+							</div><!-- .container -->
+						</div><!-- .item -->
+						
+						<!-- .item -->
+							<div class="item">
+							<img src="images/banners/2022/carloan.png" alt="Car Loan">
+							<div class="container">
+								<div class="slider-content">
+								<div class="button-action">
+										<a href="pdf/loan-application.pdf" class="button primary rounded" target="_blank" aria-label="Learn More About Car Loans" style="    margin-top: -60px;">Learn More</a>
+									</div>
+								</div>
+							</div><!-- .container -->
+						</div><!-- .item -->
+						
+						
+						<!-- .item -->
+							<div class="item">
+							<img src="images/banners/2022/creditcard.png" alt="Credit Card">
+							<div class="container">
+								<div class="slider-content">
+								<div class="button-action">
+										<a href="pages/credit-cards.php" class="button primary rounded" target="_blank" aria-label="Learn More About Credit Cards" style="    margin-top: 200px;">Learn More</a>
+									</div>
+								</div>
+							</div><!-- .container -->
+						</div><!-- .item -->
+						
+						
+						<!-- .item -->
+							<div class="item">
+							<img src="images/banners/2022/fieldofmembership.png" alt="Membership">
+							<div class="container">
+								<div class="slider-content">
+								<div class="button-action">
+										<a href="pages/who-can-join.php" class="button primary rounded" target="_blank" aria-label="Learn More About Memberships" style="    margin-top: -120px;">Learn More</a>
+									</div>
+								</div>
+							</div><!-- .container -->
+						</div><!-- .item -->
+						
+						
+						
+						<!-- .item -->
+							<div class="item">
+							<img src="images/banners/2022/zelle.png" alt="Zelle">
+							<div class="container">
+								<div class="slider-content">
+								<div class="button-action">
+										<a href="pages/zelle.php" class="button primary rounded" target="_blank" aria-label="Learn More About Zelle" style="    margin-top: -110px;">Learn More</a>
+									</div>
+								</div>
+							</div><!-- .container -->
+						</div><!-- .item -->
+						
+						
+						<!-- .item -->
+							<div class="item">
+							<img src="images/banners/2022/mobileapp.png" alt="Mobile App">
+							<div class="container">
+								<div class="slider-content">
+								<div class="button-action">
+										<a href="pages/online-banking.php" class="button primary rounded" target="_blank" aria-label="Learn More About Mobile App" style="    margin-top: 380px;">Learn More</a>
+									</div>
+								</div>
+							</div><!-- .container -->
+						</div><!-- .item -->
+						
+						
+							
+						
+
+					</div><!-- .slider -->
+				</section><!-- .main-slider -->
+
+				<section id="university-courses-3">
+					<div class="container">
+						<h2 class="main-title ribbon"><span>Our Services</span></h2>
+						<p class="center">Let us help you with every stage of life.</p>
+						<div class="row animation-element fade-in">
+							<div class="col-md-4 col-sm-4 col-xs-6">
+								<div class="image-box">
+									<a href="pages/loans-index.php" class="mini-zoom"><img src="images/our-services/loans.jpg" alt="Loans"></a>
+									<a href="pages/loans-index.php" class="caption">LOANS</a>
+								</div>
+							</div>
+
+							<div class="col-md-4 col-sm-4 col-xs-6">
+								<div class="image-box">
+									<a href="pages/checking-index.php" class="mini-zoom"><img src="images/our-services/checking.jpg" alt="Checking Accounts"></a>
+									<a href="pages/checking-index.php" class="caption">CHECKING</a>
+								</div>
+							</div>
+
+							<div class="col-md-4 col-sm-4 col-xs-6">
+								<div class="image-box">
+									<a href="pages/savings-index.php" class="mini-zoom"><img src="images/our-services/savings.jpg" alt="Savings Accounts"></a>
+									<a href="pages/savings-index.php" class="caption">SAVINGS</a>
+								</div>
+							</div>
+
+							<div class="col-md-4 col-sm-4 col-xs-6">
+								<div class="image-box">
+									<a href="pages/business-index.php" class="mini-zoom"><img src="images/our-services/business.jpg" alt="Business"></a>
+									<a href="pages/business-index.php" class="caption">BUSINESS</a>
+								</div>
+							</div>
+
+							<div class="col-md-4 col-sm-4 col-xs-6">
+								<div class="image-box">
+									<a href="pages/services-index.php" class="mini-zoom"><img src="images/our-services/services.jpg" alt="Services"></a>
+									<a href="pages/services-index.php" class="caption">SERVICES</a>
+								</div>
+							</div>
+
+							<div class="col-md-4 col-sm-4 col-xs-6">
+								<div class="image-box">
+									<a href="pages/about-index.php" class="mini-zoom"><img src="images/our-services/who-we-are.jpg" alt="About Us"></a>
+									<a href="pages/about-index.php" class="caption">ABOUT US</a>
+								</div>
+							</div>
+						</div><!-- .row -->
+					</div><!-- .container -->
+				</section><!-- #university-courses-3 -->
+
+			</main><!-- .site-main -->
+		</div><!-- .site-content -->
+
+		<?php include 'inc/footer.php';?>
+						
+	</div><!-- #wrapper -->
+	<?php include 'inc/modals.php';?>
+	<?php include 'inc/scripts.php';?>
+
+</body>
+</html>
