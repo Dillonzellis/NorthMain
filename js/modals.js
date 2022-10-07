84b36003d18628709939506329be21b0fb91ac4a
Dillon Ellis <dillonzellis@gmail.com>
2022-10-07 10:13:23 -0400

    add fb and insta modals

---
+++ js/modals.js
@@ -0,0 +1,25 @@
+// fb
+const fbModal = document.querySelector('#fb-modal')
+
+const openFbModal= document.querySelector('.open-fb-modal');
+const closeFbModal= document.querySelector('.close-fb-modal');
+
+openFbModal.addEventListener('click', ()=>{
+    fbModal.showModal();
+})
+closeFbModal.addEventListener('click', ()=>{
+    fbModal.close();
+})
+
+// instagram
+const instaModal = document.querySelector('#insta-modal')
+
+const openinstaModal= document.querySelector('.open-insta-modal');
+const closeinstaModal= document.querySelector('.close-insta-modal');
+
+openinstaModal.addEventListener('click', ()=>{
+    instaModal.showModal();
+})
+closeinstaModal.addEventListener('click', ()=>{
+    instaModal.close();
+})
